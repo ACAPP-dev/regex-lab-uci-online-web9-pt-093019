6e1a107dee6d87e063adc20ac510eef2b55f00a0
Andrew Capp <acapp909@gmail.com>
2019-10-25 03:22:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,10 +12,15 @@ end
 def words_five_letters_long(string)
 
   string.scan(/\b\w{5}\b/)
-    
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(string)
   string.match?(/\A[A-Z].+[.,!,?]$/)
   #binding.pry
+end
+
+def valid_phone_number?(number)
+  number.match(/[0-9][0-9][0-9].?[0-9][0-9][0-9].?[0-9][0-9][0-9][0-9]/)
+
 end
