9d3f9d6a676be8bae8d09318779b085b7d7d375a
Andrew Capp <acapp909@gmail.com>
2019-10-25 02:46:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,10 @@ end
 def words_starting_with_un_and_ending_with_ing(string)
   return_array = []
 
-  
+  string.split.each do |word|
+    return_array = word.scan(/\A(un)\w+(ing)$/)
+
+  end
 
 
 end
