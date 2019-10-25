8590ce0bd731e3a1153aec2c300632e73c2517e4
Andrew Capp <acapp909@gmail.com>
2019-10-25 02:48:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def starts_with_a_vowel?(string)
   string.match?(/\A[a,e,i,o,u,A,E,I,O,U]/)
 end
@@ -7,7 +8,7 @@ def words_starting_with_un_and_ending_with_ing(string)
 
   string.split.each do |word|
     return_array = word.scan(/\A(un)\w+(ing)$/)
-
+    binding.pry
   end
 
 
